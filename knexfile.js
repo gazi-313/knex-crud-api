790e1379dd9f1759be5e784852d5a38d791bb4bd
gazi-313 <gazi.tarique@intelletive.com>
2020-03-31 19:31:22 +0600

    Build seed.

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      database: 'knextestdb',
+      user: 'postgres',
+      password: 'password'
+    }
+  },
+};
