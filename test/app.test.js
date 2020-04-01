85f0f3bc3d9800b11f8fd49263018b435b1469f7
gazi-313 <gazi.tarique@intelletive.com>
2020-04-01 12:52:50 +0600

    Finished setup test.

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,29 @@
+const request = require('supertest');
+const knex = require('../db/knex');
+const app = require('../app');
+const { response } = require('../app');
+//const { request } = require('express');
+const fixtures = require('./fixtures');
 describe('CRUD Stickers', () => {
-    before(() => {
+    before((done) => {
         knex.migrate.latest().then(()=>{
             return knex.seed.run();
-        });
+        }).then(() => done());
+    });
+    it('Works...', function(){
+        console.log('Its working');
+    });
+
+    it('Lists all records', (done) => {
+        request(app)
+            .get('/api/v1/stickers')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', '/json/')
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('array');
+                expect(response.body).to.deep.equal(fixtures.stickers);
+                done();
+            });
     });
 });
---
+++ test/fixtures.js
@@ -0,0 +1,29 @@
+const stickers = [{
+    title: "title 1",
+    description: "description 1",
+    rating: 10,
+    url: 'url1'
+},
+{
+    title: "title 2",
+    description: "description 2",
+    rating: 8,
+    url: 'url2'
+},
+{
+    title: "title 3",
+    description: "description 3",
+    rating: 5,
+    url: 'url3'
+},
+{
+    title: "title 4",
+    description: "description 4",
+    rating: 7,
+    url: 'url4'
+}];
+
+
+module.exports = {
+    stickers
+}
